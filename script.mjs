8ee400ec8f3b7a13b8fee0af6879d50c7eab10a0
LiliyaLexx <liliya.fedyurina@gmail.com>
2024-05-03 16:57:11 -0700

    2nd part is added

--- script.mjs
+++ script.mjs
@@ -57,7 +57,68 @@ console.log(((num2-num1)*num3)%num4);
 //Change the way that isOver25 calculates so that we do not need to use the NOT operator (!) in other logic comparisons. Rename the variable as appropriate.
 let isUnder25  = num1<25 || num2 < 25 || num3 < 25|| num4 < 25;
 
+console.log(isUnder25);
+
 // Part 2: Practical Math
+
+// Constants
+const distance = 1500; // miles
+const budget = 175; // dollars
+const fuelCostPerGallon = 3; // dollars
+const fuelEfficiency = {
+    55: 30, // miles per gallon
+    60: 28,
+    75: 23
+};
+
+// Function to calculate fuel required
+function fuel(speed) {
+    return distance / fuelEfficiency[speed];
+}
+
+// Function to calculate trip duration
+function trip(speed) {
+    return distance / speed;
+}
+
+// Calculate fuel required and trip duration for each speed
+for (let speed in fuelEfficiency) {
+    const fuelRequired = fuel(speed);
+    const tripDuration = trip(speed);
+    const fuelExpense = fuel * fuelCostPerGallon;
+
+    console.log(`At ${speed} mph:`);
+    console.log(`  Fuel required: ${fuelRequired.toFixed(2)} gallons`);
+    console.log(`  Trip duration: ${tripDuration.toFixed(2)} hours`);
+    console.log(`  Fuel expense: $${fuelExpense.toFixed(2)}`);
+
+    if (fuelExpense <= budget) {
+        console.log("  Budget is enough to cover the fuel expense.");
+    } else {
+        console.log("  Budget is NOT enough to cover the fuel expense.");
+    }
+}
+
+// const totalDistance = 1500;
+// const fuelBudject = 175;
+// const costFuel = 3;
+
+// // 3How long will the trip take, in hours?
+// let trip = 1500/55;
+// let trip2 = 1500/60;
+// let trip3 = 1500/75;
+// console.log(trip, trip2, trip3);
+
+// // Will your budget be enough to cover the fuel expense?
+
+// let budjet1 = trip*costFuel;
+// let budjet2 = trip2*costFuel;
+// let budjet3 = trip3 *costFuel;
+// console.log(budjet1, budjet2, budjet3);
+
+// // How many gallons of fuel will you need for the entire trip?
+
+
 
 
 
